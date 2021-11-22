7b4fd8a6fa7380ca7d9a6e58ad5a392e157ceae8
Sam Lynch <samdawkins1998@gmail.com>
2021-11-22 18:29:26 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1=31;
+let num2=2;
+let num3 = 16
+let num4 = 12
+let multiply = num1*num2
 
-// Write your code here
+
+let random = Math.floor(Math.random()*10);
+let mod = num3 % num4
+
+let max = Math.max(1, 2, 4, 20)
